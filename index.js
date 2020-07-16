849d14f4af63350d741eae63047fe24c5efbcfe0
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2020-07-16 19:10:49 +0200

    Generate random participant responses

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const chance = require("chance")
+//Step 1. Generating random responses for 200 participants - need for generate participant ID as well?
+    //SWEBO - for all 10 items
+    chance.integer({ min: 1, max: 4 })
+    //BDI - for all 21 items
+    chance.integer({ min : 0, max: 3 })
+    //PCL - for all 20 items
+    chance.integer({ min: 0, max : 4 })
+    //FAKE - for all 10 items
+    chance.integer({ min : 0, max: 4})
+
+//Step 2. Scoring each measure
+    /*SWEBO - all items are REVERSED scored, where higher numbers mean higher burnout
+       so this means that for ever 4, this should be scored as a 1, and for evert 3 it should be scored as a 2, etc. */
+    
+   /*BDI -  */
