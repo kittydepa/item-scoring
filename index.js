918bf9b436d24cf08e7c42331e086f5b38b785fb
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2020-07-17 15:55:23 +0200

    Add comments on how to score each questionnaire

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const chance = require("chance")
+const chance = require("chance")()
 //Step 1. Generating random responses for 200 participants - need for generate participant ID as well?
+//How to get this applied to 200 ppl
     //SWEBO - for all 10 items
-    chance.integer({ min: 1, max: 4 })
+    //chance.integer({ min: 1, max: 4 })
+    console.log(chance.integer({ min: 1, max: 4 }))
     //BDI - for all 21 items
     chance.integer({ min : 0, max: 3 })
     //PCL - for all 20 items
@@ -10,7 +12,14 @@ const chance = require("chance")
     chance.integer({ min : 0, max: 4})
 
 //Step 2. Scoring each measure
-    /*SWEBO - all items are REVERSED scored, where higher numbers mean higher burnout
-       so this means that for ever 4, this should be scored as a 1, and for evert 3 it should be scored as a 2, etc. */
+    /*SWEBO - all items are REVERSE scored, where higher numbers mean higher burnout
+       so this means that for ever 4, this should be scored as a 1, and for every 3 it should be scored as a 2, etc. */
     
-   /*BDI -  */
+   /*BDI -  all items. 
+   Here, we also want to know the severity of depression, where Mild = 0-13, Moderate = 11-19, Severe = 28.*/
+   
+   /* PCL - all items.
+    The cut-off for PTSD is 31. */
+
+    /* FAKE - all items. 
+    Note that items 3, 5, and 7 are REVERSE scored.   */
